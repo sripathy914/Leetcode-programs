dc7883885920b173cfb21667b9c1baf6432771a5
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-01-23 21:31:11 +0530

    Create 151. Reverse Words in a String .java

---
+++ easy/151. Reverse Words in a String .java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public String reverseWords(String s)
+    {
+        String str[]=s.trim().split("\\s+");
+        String res="";
+        for(int i=str.length-1;i>0;i--)
+        {
+            res+=str[i]+" ";
+        }
+        return res+str[0];
+    }
+}
